110441287bd065b717dd4a0a2688a1ac39983fdd
Clancastle <dennisungureanu0@gmail.com>
2023-07-13 21:40:58 -0500

    sum up
    the code works now, links in nav menu, and some descriptions of things, and the jija templating works.

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,21 @@
 from flask import Blueprint, render_template
 
-auth = Blueprint("auth", __name__)
+"""
+authentication folder, for signing up, loging in and out. 
+
+"""
+
+auth = Blueprint("auth", __name__) #doesnt have to have auth name var, and auth in blueprint
 
 # http://127.0.0.1:5000/a/ (route)
 
-@auth.route('/login')
+@auth.route('/login') #makes a new route(website/login), and we give that route html we made in a differnet file
 def login():
     return render_template('login.html')
 
-# @auth.route('/logout')
-# def logout():
-#     return render_template
+@auth.route('/logout')
+def logout():
+    return render_template('login.html')
 
 @auth.route('/signup')
 def signup():
