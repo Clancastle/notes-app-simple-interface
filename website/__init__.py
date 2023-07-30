49c02be5801f7310369c2934d682c71c567d4636
Clancastle <dennisungureanu0@gmail.com>
2023-07-29 23:30:21 -0500

    update?

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "dennis"  # makes secret key, encrypts
